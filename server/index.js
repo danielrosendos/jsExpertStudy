1f6e4c574a3f0cb7c72ed7c0eb422ae0ce08fdba
Daniel Rosendo <drosendo@digitahub.com.br>
2022-03-15 16:47:31 -0300

    adiciona log pino

--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
 import server from "./server.js";
+import { logger } from "./util.js";
 
 server.listen(3000).on('listening', () => {
-    console.log('Server running!!')
+    logger.info('Server running!!')
 })
