b89f058811ca963ebe9a9744b291e43c7bd00e2b
Daniel Rosendo <drosendo@digitahub.com.br>
2022-03-15 17:05:55 -0300

    criação do arquivo service

---
+++ server/service.js
@@ -0,0 +1,55 @@
+import fs from 'fs'
+import fsPromises from 'fs/promises'
+
+import config from "./config.js";
+import { join, extname } from 'path'
+
+const {
+    dir: {
+        publicDirectory
+    }
+} = config;
+
+export class Service {
+    /**
+     *
+     * @param filename
+     */
+    createFileStream(filename) {
+        fs.createFileStream(filename)
+    }
+
+    /**
+     *
+     * @param filename
+     * @returns {Promise<{name: *, type: *}>}
+     */
+    async getFileInfo(filename) {
+        const fullFilePath = join(publicDirectory, filename)
+        // valida se existe, se não existe estoura erro!!
+        await fsPromises.access(fullFilePath)
+        const fileType = extname(fullFilePath)
+
+        return {
+            type: fileType,
+            name: fullFilePath
+        }
+    }
+
+    /**
+     *
+     * @param filename
+     * @returns {Promise<{stream: void, type: *}>}
+     */
+    async getFileStream(filename) {
+        const {
+            name,
+            type
+        } = await this.getFileInfo(filename)
+
+        return {
+            stream: this.createFileStream(name),
+            type
+        }
+    }
+}
